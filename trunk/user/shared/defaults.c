eeb78062f68744445e0cf54be097b4bd4de8c278
CC <59593391+CC13594759@users.noreply.github.com>
2023-09-19 22:27:44 +0800

    Update defaults.c

--- trunk/user/shared/defaults.c
+++ trunk/user/shared/defaults.c
@@ -68,8 +68,8 @@ struct nvram_pair router_defaults[] = {
 	{ "wan_ipaddr", "0.0.0.0" },		/* WAN IP address */
 	{ "wan_netmask", "0.0.0.0" },		/* WAN netmask */
 	{ "wan_gateway", "0.0.0.0" },		/* WAN gateway */
-	{ "wan_dnsenable_x", "0" },
-	{ "wan_dns1_x", "223.5.5.5" },
+	{ "wan_dnsenable_x", "1" },
+	{ "wan_dns1_x", "" },
 	{ "wan_dns2_x", "" },
 	{ "wan_dns3_x", "" },
 	{ "wan_hostname", "" },			/* WAN hostname */
