66d0f29d57f867fd553582e23876f7b71d0e05dc
CC <59593391+CC13594759@users.noreply.github.com>
2023-11-03 10:00:47 +0800

    Update defaults.c

--- trunk/user/shared/defaults.c
+++ trunk/user/shared/defaults.c
@@ -54,8 +54,8 @@ struct nvram_pair router_defaults[] = {
 	{ "lan_netmask", DEF_LAN_MASK },	/* LAN netmask */
 	{ "lan_gateway", DEF_LAN_ADDR },	/* LAN gateway */
 	{ "lan_dns_x", "1" },			/* LAN DNS [static|dhcp] */
-	{ "lan_dns1", "" },			/* LAN DNS1 */
-	{ "lan_dns2", "" },			/* LAN DNS2 */
+	{ "lan_dns1", "223.5.5.5" }，		/* LAN DNS1 */
+	{ "lan_dns2", "114.114.114.114" }，	/* LAN DNS2 */
 	{ "lan_domain", "lan" },		/* LAN domain name */
 	{ "lan_stp", "1" },			/* LAN spanning tree protocol */
 
@@ -236,7 +236,7 @@ struct nvram_pair router_defaults[] = {
 	{ "wl_HT_BAWinSize", "64" },
 #endif
 	{ "wl_HT_80211KV", "1" },
-	{ "wl_HT_80211R", "0" },
+	{ "wl_HT_80211R", "1" },
 	{ "wl_HT_MpduDensity", "5" },
 	{ "wl_HT_AutoBA", "1" },
 	{ "wl_VgaClamp", "0" },
@@ -335,7 +335,7 @@ struct nvram_pair router_defaults[] = {
 	{ "rt_HT_AMSDU", "0" },
 	{ "rt_HT_MpduDensity", "5" },
 	{ "rt_HT_80211KV", "1" },
-	{ "rt_HT_80211R", "0" },
+	{ "rt_HT_80211R", "1" },
 #if defined (USE_WID_2G) && (USE_WID_2G==7615 || USE_WID_2G==7915)
 	{ "rt_HT_BAWinSize", "256" },
 	{ "rt_ldpc", "1" },
